acbfc56f2679a5344f2bf0c0abf83f894c33ab5b
lbarthon <lsbarthonet2@gmail.com>
2018-12-27 15:05:19 +0100

    d01 -- ex00 ex01 ex02 good

---
+++ d01/ex02/oddeven.php
@@ -0,0 +1,18 @@
+#!/usr/bin/php
+<?php
+    while (1)
+    {
+        echo "Entrez un nombre: ";
+        $read = trim(fgets(STDIN));
+        if (feof(STDIN)) exit();
+        if (!is_numeric($read)) {
+            printf("'$read' n'est pas un chiffre\n");
+        } else {
+            if ($read % 2 == 1) {
+                printf ("Le nombre $read est Impair\n");
+            } else {
+                printf ("Le nombre $read est Pair\n");
+            }
+        }
+    }
+?>
