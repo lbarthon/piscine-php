2568264aa91091707603b94f6e9ef48ba94a5155
lbarthon <lsbarthonet2@gmail.com>
2019-01-04 16:08:19 +0100

    oddeven.php fixed (d01 - ex02)

--- d01/ex02/oddeven.php
+++ d01/ex02/oddeven.php
@@ -8,7 +8,7 @@
         if (!is_numeric($read)) {
             printf("'$read' n'est pas un chiffre\n");
         } else {
-            if ($read % 2 == 1) {
+            if ($read % 2 == 1 || $read % 2 == -1) {
                 printf ("Le nombre $read est Impair\n");
             } else {
                 printf ("Le nombre $read est Pair\n");
