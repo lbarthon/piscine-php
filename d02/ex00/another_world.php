479b2cee9cf781806d111f4f5bd2b1ecccea7a0b
lbarthon <lsbarthonet2@gmail.com>
2019-01-11 17:29:48 +0100

    yes

--- d02/ex00/another_world.php
+++ d02/ex00/another_world.php
@@ -5,7 +5,7 @@
         $array = preg_split($regexp, trim($argv[1]));
         $len = count($array);
         $i = 1;
-        foreach ($array as $value) {
+            foreach ($array as $value) {
             echo $value . ($i === $len ? "\n" : " ");
             $i++;
         }
