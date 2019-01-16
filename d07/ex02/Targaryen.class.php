a7d74d93a1448898928cf5189db1a30ca5f3f5f6
lbarthon <lsbarthonet2@gmail.com>
2019-01-16 09:31:55 +0100

    d07 -- Ex00 to ex04 done and tested

---
+++ d07/ex02/Targaryen.class.php
@@ -0,0 +1,14 @@
+<?php
+    class Targaryen {
+
+        function resistsFire() {
+            return false;
+        }
+
+        function getBurned() {
+            if ($this->resistsFire()) {
+                return "emerges naked but unharmed";
+            }
+            return "burns alive";
+        }
+    }
